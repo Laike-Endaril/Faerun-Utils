24c543b4488e11d4bf084a32700ea676a26dab95
Kyle Koder <silvertallon@gmail.com>
2019-08-19 23:00:45 -0700

    Simplified the GUI class even further

--- src/main/java/com/fantasticsource/faerunutils/crafting/GUIBetterCrafting.java
+++ src/main/java/com/fantasticsource/faerunutils/crafting/GUIBetterCrafting.java
@@ -4,8 +4,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.inventory.ClickType;
-import net.minecraft.inventory.Slot;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.relauncher.Side;
@@ -21,42 +19,25 @@ public class GUIBetterCrafting extends GuiContainer
         super(new ContainerBetterCraftingTable(Minecraft.getMinecraft().player.inventory, Minecraft.getMinecraft().world, BlockPos.ORIGIN));
     }
 
-    public void initGui()
-    {
-        super.initGui();
-    }
-
-    public void updateScreen()
-    {
-        super.updateScreen();
-    }
-
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
-        this.drawDefaultBackground();
+        drawDefaultBackground();
 
         super.drawScreen(mouseX, mouseY, partialTicks);
 
-        this.renderHoveredToolTip(mouseX, mouseY);
+        renderHoveredToolTip(mouseX, mouseY);
     }
 
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
     {
-        this.fontRenderer.drawString(I18n.format("container.crafting"), 28, 6, 4210752);
-        this.fontRenderer.drawString(I18n.format("container.inventory"), 8, this.ySize - 96 + 2, 4210752);
+        fontRenderer.drawString(I18n.format("container.crafting"), 28, 6, 4210752);
+        fontRenderer.drawString(I18n.format("container.inventory"), 8, ySize - 96 + 2, 4210752);
     }
 
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
     {
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        this.mc.getTextureManager().bindTexture(CRAFTING_TABLE_GUI_TEXTURES);
-        int i = this.guiLeft;
-        int j = (this.height - this.ySize) / 2;
-        this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
-    }
-
-    protected void handleMouseClick(Slot slotIn, int slotId, int mouseButton, ClickType type)
-    {
-        super.handleMouseClick(slotIn, slotId, mouseButton, type);
+        GlStateManager.color(1, 1, 1, 1);
+        mc.getTextureManager().bindTexture(CRAFTING_TABLE_GUI_TEXTURES);
+        drawTexturedModalRect(guiLeft, (height - ySize) / 2, 0, 0, xSize, ySize);
     }
 }
