01cfef0a2bd29b137ec6f0e8c0fbbd825298a0eb
Kyle Koder <silvertallon@gmail.com>
2019-08-19 22:57:06 -0700

    Removed the recipe book

--- src/main/java/com/fantasticsource/faerunutils/crafting/GUIBetterCrafting.java
+++ src/main/java/com/fantasticsource/faerunutils/crafting/GUIBetterCrafting.java
@@ -1,11 +1,7 @@
 package com.fantasticsource.faerunutils.crafting;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiButtonImage;
 import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.gui.recipebook.GuiRecipeBook;
-import net.minecraft.client.gui.recipebook.IRecipeShownListener;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.inventory.ClickType;
@@ -15,56 +11,33 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import java.io.IOException;
-
 @SideOnly(Side.CLIENT)
-public class GUIBetterCrafting extends GuiContainer implements IRecipeShownListener
+public class GUIBetterCrafting extends GuiContainer
 {
     private static final ResourceLocation CRAFTING_TABLE_GUI_TEXTURES = new ResourceLocation("textures/gui/container/crafting_table.png");
-    private final GuiRecipeBook recipeBookGui;
-    private GuiButtonImage recipeButton;
-    private boolean widthTooNarrow;
 
     public GUIBetterCrafting()
     {
         super(new ContainerBetterCraftingTable(Minecraft.getMinecraft().player.inventory, Minecraft.getMinecraft().world, BlockPos.ORIGIN));
-        this.recipeBookGui = new GuiRecipeBook();
     }
 
     public void initGui()
     {
         super.initGui();
-        this.widthTooNarrow = this.width < 379;
-        this.recipeBookGui.func_194303_a(this.width, this.height, this.mc, this.widthTooNarrow, ((ContainerBetterCraftingTable) this.inventorySlots).craftMatrix);
-        this.guiLeft = this.recipeBookGui.updateScreenPosition(this.widthTooNarrow, this.width, this.xSize);
-        this.recipeButton = new GuiButtonImage(10, this.guiLeft + 5, this.height / 2 - 49, 20, 18, 0, 168, 19, CRAFTING_TABLE_GUI_TEXTURES);
-        this.buttonList.add(this.recipeButton);
     }
 
     public void updateScreen()
     {
         super.updateScreen();
-        this.recipeBookGui.tick();
     }
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
         this.drawDefaultBackground();
 
-        if (this.recipeBookGui.isVisible() && this.widthTooNarrow)
-        {
-            this.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
-            this.recipeBookGui.render(mouseX, mouseY, partialTicks);
-        }
-        else
-        {
-            this.recipeBookGui.render(mouseX, mouseY, partialTicks);
-            super.drawScreen(mouseX, mouseY, partialTicks);
-            this.recipeBookGui.renderGhostRecipe(this.guiLeft, this.guiTop, true, partialTicks);
-        }
+        super.drawScreen(mouseX, mouseY, partialTicks);
 
         this.renderHoveredToolTip(mouseX, mouseY);
-        this.recipeBookGui.renderTooltip(this.guiLeft, this.guiTop, mouseX, mouseY);
     }
 
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
@@ -82,66 +55,8 @@ public class GUIBetterCrafting extends GuiContainer implements IRecipeShownListe
         this.drawTexturedModalRect(i, j, 0, 0, this.xSize, this.ySize);
     }
 
-    protected boolean isPointInRegion(int rectX, int rectY, int rectWidth, int rectHeight, int pointX, int pointY)
-    {
-        return (!this.widthTooNarrow || !this.recipeBookGui.isVisible()) && super.isPointInRegion(rectX, rectY, rectWidth, rectHeight, pointX, pointY);
-    }
-
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException
-    {
-        if (!this.recipeBookGui.mouseClicked(mouseX, mouseY, mouseButton))
-        {
-            if (!this.widthTooNarrow || !this.recipeBookGui.isVisible())
-            {
-                super.mouseClicked(mouseX, mouseY, mouseButton);
-            }
-        }
-    }
-
-    protected boolean hasClickedOutside(int p_193983_1_, int p_193983_2_, int p_193983_3_, int p_193983_4_)
-    {
-        boolean flag = p_193983_1_ < p_193983_3_ || p_193983_2_ < p_193983_4_ || p_193983_1_ >= p_193983_3_ + this.xSize || p_193983_2_ >= p_193983_4_ + this.ySize;
-        return this.recipeBookGui.hasClickedOutside(p_193983_1_, p_193983_2_, this.guiLeft, this.guiTop, this.xSize, this.ySize) && flag;
-    }
-
-    protected void actionPerformed(GuiButton button)
-    {
-        if (button.id == 10)
-        {
-            this.recipeBookGui.initVisuals(this.widthTooNarrow, ((ContainerBetterCraftingTable) this.inventorySlots).craftMatrix);
-            this.recipeBookGui.toggleVisibility();
-            this.guiLeft = this.recipeBookGui.updateScreenPosition(this.widthTooNarrow, this.width, this.xSize);
-            this.recipeButton.setPosition(this.guiLeft + 5, this.height / 2 - 49);
-        }
-    }
-
-    protected void keyTyped(char typedChar, int keyCode) throws IOException
-    {
-        if (!this.recipeBookGui.keyPressed(typedChar, keyCode))
-        {
-            super.keyTyped(typedChar, keyCode);
-        }
-    }
-
     protected void handleMouseClick(Slot slotIn, int slotId, int mouseButton, ClickType type)
     {
         super.handleMouseClick(slotIn, slotId, mouseButton, type);
-        this.recipeBookGui.slotClicked(slotIn);
-    }
-
-    public void recipesUpdated()
-    {
-        this.recipeBookGui.recipesUpdated();
-    }
-
-    public void onGuiClosed()
-    {
-        this.recipeBookGui.removed();
-        super.onGuiClosed();
-    }
-
-    public GuiRecipeBook func_194310_f()
-    {
-        return this.recipeBookGui;
     }
 }
