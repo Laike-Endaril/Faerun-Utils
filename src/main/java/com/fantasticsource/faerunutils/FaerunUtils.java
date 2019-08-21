af4e73dc2b173c5a6265580aaa93d4c5653087be
Kyle Koder <silvertallon@gmail.com>
2019-08-21 04:13:22 -0700

    1.12.2.002a

--- src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
+++ src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
@@ -18,7 +18,7 @@ public class FaerunUtils
 {
     public static final String MODID = "faerunutils";
     public static final String NAME = "Faerun Utils";
-    public static final String VERSION = "1.12.2.002";
+    public static final String VERSION = "1.12.2.002a";
 
     public static boolean faerun;
 
