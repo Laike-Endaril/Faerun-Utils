a9ff7333cba3033dfd4919f5d5b22331f2d0653d
Kyle Koder <silvertallon@gmail.com>
2019-08-22 07:43:45 -0700

    Arrow buttons for selecting recipe (no backend yet)

--- src/main/java/com/fantasticsource/faerunutils/bettercrafting/table/GUIBetterCrafting.java
+++ src/main/java/com/fantasticsource/faerunutils/bettercrafting/table/GUIBetterCrafting.java
@@ -1,6 +1,7 @@
 package com.fantasticsource.faerunutils.bettercrafting.table;
 
 import com.fantasticsource.faerunutils.FaerunUtils;
+import com.fantasticsource.tools.Collision;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.renderer.BufferBuilder;
@@ -17,11 +18,9 @@ import static com.fantasticsource.faerunutils.FaerunUtils.faerun;
 @SideOnly(Side.CLIENT)
 public class GUIBetterCrafting extends GuiContainer
 {
-    private static final ResourceLocation TEXTURE = new ResourceLocation("textures/gui/container/crafting_table.png");
+    private static final ResourceLocation TEXTURE = new ResourceLocation(FaerunUtils.MODID, "textures/gui/bettercraftingtable.png");
 
     private static final ResourceLocation TEXTURE_FAERUN = new ResourceLocation(FaerunUtils.MODID, "textures/gui/bettercraftingtablefaerun.png");
-
-    private final double halfPixel;
 
     public GUIBetterCrafting()
     {
@@ -32,8 +31,6 @@ public class GUIBetterCrafting extends GuiContainer
             xSize = 248;
             ySize = 202;
         }
-
-        halfPixel = 0.5 / 256;
     }
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
@@ -57,13 +54,59 @@ public class GUIBetterCrafting extends GuiContainer
         int x2 = x1 + xSize;
         int y2 = y1 + ySize;
 
+
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferbuilder = tessellator.getBuffer();
+
+        //Main background
         bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
-        bufferbuilder.pos(x1, y2, zLevel).tex(halfPixel, (double) ySize / 256 - halfPixel).endVertex();
-        bufferbuilder.pos(x2, y2, zLevel).tex((double) xSize / 256 - halfPixel, (double) ySize / 256 - halfPixel).endVertex();
-        bufferbuilder.pos(x2, y1, zLevel).tex((double) xSize / 256 - halfPixel, halfPixel).endVertex();
-        bufferbuilder.pos(x1, y1, zLevel).tex(halfPixel, halfPixel).endVertex();
+        bufferbuilder.pos(x1, y2, zLevel).tex(0, (double) ySize / 256).endVertex();
+        bufferbuilder.pos(x2, y2, zLevel).tex((double) xSize / 256, (double) ySize / 256).endVertex();
+        bufferbuilder.pos(x2, y1, zLevel).tex((double) xSize / 256, 0).endVertex();
+        bufferbuilder.pos(x1, y1, zLevel).tex(0, 0).endVertex();
+        tessellator.draw();
+
+
+        //Up arrow
+        x1 += 152;
+        y1 += 31;
+        x2 = x1 + 8;
+        y2 = y1 + 10;
+
+        double u1;
+        if (Collision.pointRectangle(mouseX, mouseY, x1, y1, x2, y2)) u1 = 232; //active
+        else u1 = 224; //inactive
+
+        double v1 = 240, u2 = u1 + 8, v2 = v1 + 10;
+
+        u1 /= 256;
+        v1 /= 256;
+        u2 /= 256;
+        v2 /= 256;
+
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos(x1, y2, zLevel).tex(u1, v2).endVertex();
+        bufferbuilder.pos(x2, y2, zLevel).tex(u2, v2).endVertex();
+        bufferbuilder.pos(x2, y1, zLevel).tex(u2, v1).endVertex();
+        bufferbuilder.pos(x1, y1, zLevel).tex(u1, v1).endVertex();
+        tessellator.draw();
+
+
+        //Down arrow
+        y1 += 13;
+        y2 += 13;
+
+        if (Collision.pointRectangle(mouseX, mouseY, x1, y1, x2, y2)) u1 = 248; //active
+        else u1 = 240; //inactive
+
+        u2 = (u1 + 8) / 256;
+        u1 /= 256;
+
+        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX);
+        bufferbuilder.pos(x1, y2, zLevel).tex(u1, v2).endVertex();
+        bufferbuilder.pos(x2, y2, zLevel).tex(u2, v2).endVertex();
+        bufferbuilder.pos(x2, y1, zLevel).tex(u2, v1).endVertex();
+        bufferbuilder.pos(x1, y1, zLevel).tex(u1, v1).endVertex();
         tessellator.draw();
     }
 }
