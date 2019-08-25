c03d5bb97a94239a9f25eec398d33267b17977bf
Kyle Koder <silvertallon@gmail.com>
2019-08-25 11:34:16 -0700

    1.12.2.003e

--- src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
+++ src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
@@ -29,12 +29,12 @@ import net.minecraftforge.registries.ForgeRegistry;
 
 import java.io.IOException;
 
-@Mod(modid = FaerunUtils.MODID, name = FaerunUtils.NAME, version = FaerunUtils.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021c,)", acceptableRemoteVersions = "[1.12.2.003,1.12.2.003d]")
+@Mod(modid = FaerunUtils.MODID, name = FaerunUtils.NAME, version = FaerunUtils.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.021c,)", acceptableRemoteVersions = "[1.12.2.003,1.12.2.003e]")
 public class FaerunUtils
 {
     public static final String MODID = "faerunutils";
     public static final String NAME = "Faerun Utils";
-    public static final String VERSION = "1.12.2.003d";
+    public static final String VERSION = "1.12.2.003e";
 
     public static boolean faerun;
 
