703e02a71abcf0e1aae7d9feb4ade6162716d49d
Kyle Koder <silvertallon@gmail.com>
2020-10-16 13:16:51 -0700

    Added Tiamat Inventory and req

--- src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
+++ src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = FaerunUtils.MODID, name = FaerunUtils.NAME, version = FaerunUtils.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.038e,)")
+@Mod(modid = FaerunUtils.MODID, name = FaerunUtils.NAME, version = FaerunUtils.VERSION, dependencies = "required-after:fantasticlib@[1.12.2.038e,);required-after:tiamatinventory@[1.12.2.000zc,)")
 public class FaerunUtils
 {
     public static final String MODID = "faerunutils";
