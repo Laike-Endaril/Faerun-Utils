88487d5efdb2d16fc6ab679b284f6f4412dbc5b2
Kyle Koder <silvertallon@gmail.com>
2021-01-22 23:33:32 -0800

    Fixed being able to delete items by putting them in "your" slots

--- src/main/java/com/fantasticsource/faerunutils/interaction/trading/YourSlot.java
+++ src/main/java/com/fantasticsource/faerunutils/interaction/trading/YourSlot.java
@@ -1,15 +1,15 @@
 package com.fantasticsource.faerunutils.interaction.trading;
 
-import com.fantasticsource.mctools.inventory.slot.BetterSlot;
+import com.fantasticsource.mctools.inventory.slot.FilteredSlot;
 import net.minecraft.entity.player.EntityPlayer;
 
-public class YourSlot extends BetterSlot
+public class YourSlot extends FilteredSlot
 {
     private final ContainerTrade container;
 
     public YourSlot(ContainerTrade container, int index, int x, int y, int u, int v)
     {
-        super(container.inventory, index, x, y, ContainerTrade.TEXTURE, 256, 256, u, v);
+        super(container.inventory, index, x, y, ContainerTrade.TEXTURE, 256, 256, u, v, false, 64, stack -> false);
         this.container = container;
     }
 
