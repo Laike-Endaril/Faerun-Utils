ac55b87aeddd8aeb5aff8436a13602afbe01b079
Kyle Koder <silvertallon@gmail.com>
2021-03-21 12:08:05 -0700

    Removed deep wounds application

--- src/main/java/com/fantasticsource/faerunutils/potions/PotionDeepWounds.java
+++ src/main/java/com/fantasticsource/faerunutils/potions/PotionDeepWounds.java
@@ -31,18 +31,18 @@ public class PotionDeepWounds extends BetterPotion
     }
 
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void livingHurt(LivingDamageEvent event)
-    {
-        EntityLivingBase entity = event.getEntityLiving();
-        if (entity instanceof EntityPlayerMP)
-        {
-            PotionEffect effect = entity.getActivePotionEffect(PotionDefinitions.POTION_EFFECT_DEEP_WOUNDS);
-            int level = (int) ((effect == null ? 0 : effect.getAmplifier() + 1) + event.getAmount() * 0.5);
-            if (level > 0)
-            {
-                ServerTickTimer.schedule(1, () -> entity.addPotionEffect(new PotionEffect(PotionDefinitions.POTION_EFFECT_DEEP_WOUNDS, Integer.MAX_VALUE, level - 1)));
-            }
-        }
-    }
+//    @SubscribeEvent(priority = EventPriority.LOWEST)
+//    public static void livingHurt(LivingDamageEvent event)
+//    {
+//        EntityLivingBase entity = event.getEntityLiving();
+//        if (entity instanceof EntityPlayerMP)
+//        {
+//            PotionEffect effect = entity.getActivePotionEffect(PotionDefinitions.POTION_EFFECT_DEEP_WOUNDS);
+//            int level = (int) ((effect == null ? 0 : effect.getAmplifier() + 1) + event.getAmount() * 0.5);
+//            if (level > 0)
+//            {
+//                ServerTickTimer.schedule(1, () -> entity.addPotionEffect(new PotionEffect(PotionDefinitions.POTION_EFFECT_DEEP_WOUNDS, Integer.MAX_VALUE, level - 1)));
+//            }
+//        }
+//    }
 }
