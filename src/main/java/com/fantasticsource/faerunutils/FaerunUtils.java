073348f57611a70213a96cd8abcf02f9bfe225d2
Kyle Koder <silvertallon@gmail.com>
2021-04-07 19:47:02 -0700

    1.12.2.Era2.028

--- src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
+++ src/main/java/com/fantasticsource/faerunutils/FaerunUtils.java
@@ -40,7 +40,7 @@ public class FaerunUtils
 {
     public static final String MODID = "faerunutils";
     public static final String NAME = "Faerun Utils";
-    public static final String VERSION = "1.12.2.Era2.027";
+    public static final String VERSION = "1.12.2.Era2.028";
 
     @Mod.EventHandler
     public static void preInit(FMLPreInitializationEvent event)
