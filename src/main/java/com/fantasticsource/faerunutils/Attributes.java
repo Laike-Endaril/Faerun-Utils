578b091405a2eb9a1b8dec53d013ab60e75c5954
Kyle Koder <silvertallon@gmail.com>
2021-05-04 20:28:31 -0700

    Fixed some major issues with attribute heirarchy; may need to adjust how specific attributes are set up and used

--- src/main/java/com/fantasticsource/faerunutils/Attributes.java
+++ src/main/java/com/fantasticsource/faerunutils/Attributes.java
@@ -1,9 +1,7 @@
 package com.fantasticsource.faerunutils;
 
-import com.fantasticsource.mctools.betterattributes.AdditiveParentsAttribute;
 import com.fantasticsource.mctools.betterattributes.BetterAttribute;
 import com.fantasticsource.mctools.betterattributes.BetterAttributeMod;
-import com.fantasticsource.mctools.betterattributes.BonusPercentParentsAttribute;
 import com.fantasticsource.tiamathud.CustomHUDData;
 import com.fantasticsource.tools.Tools;
 import com.fantasticsource.tools.datastructures.Pair;
@@ -24,8 +22,8 @@ import static com.fantasticsource.faerunutils.FaerunUtils.MODID;
 public class Attributes
 {
     public static final BetterAttribute
-            BODY_TEMPERATURE = new BetterAttribute(MODID + ".temperature", 0), //Custom scale, not C, F, K, etc.  Status effects and damage start at +-100.  Base value is comfort temp, current value is...current.
-            COVERAGE = new BetterAttribute(MODID + ".coverage", 0), //Based on how much non-destroyed armor you're wearing.  100% with any full set of unbroken armor
+            BODY_TEMPERATURE = new BetterAttribute(MODID + ".temperature"), //Custom scale, not C, F, K, etc.  Status effects and damage start at +-100.  Base value is comfort temp, current value is...current.
+            COVERAGE = new BetterAttribute(MODID + ".coverage"), //Based on how much non-destroyed armor you're wearing.  100% with any full set of unbroken armor
 
     //All stats are balanced around a starting average of 10 and a max of 100 for each base attribute
     STRENGTH = new BetterAttribute(MODID + ".strength", 10), //Physical strength/force
@@ -35,65 +33,65 @@ public class Attributes
             MAGICAL_SKILL = new BetterAttribute(MODID + ".magicalSkill", 10), //Magical skill/precision/reflexes
             MAGICAL_CONSTITUTION = new BetterAttribute(MODID + ".magicalConstitution", 10), //Magical integrity/durability/resistance
 
-    THERMAL_RECOVERY = new AdditiveParentsAttribute(MODID + ".thermalRecovery", 5, new Pair<>(CONSTITUTION, 0.1), new Pair<>(MAGICAL_CONSTITUTION, 0.05)),
+    THERMAL_RECOVERY = new BetterAttribute(MODID + ".thermalRecovery", 5, 0, new Pair<>(CONSTITUTION, 0.1), new Pair<>(MAGICAL_CONSTITUTION, 0.05)),
 
     //Meters (blocks) per second; average human RUNNING speed is ~5, world record would be a little over 10.4, both based on a 200m dash
-    MOVE_SPEED = new AdditiveParentsAttribute(MODID + ".moveSpeed", 4.4, new Pair<>(STRENGTH, 0.02), new Pair<>(DEXTERITY, 0.02), new Pair<>(CONSTITUTION, 0.02)).setMCAttribute(SharedMonsterAttributes.MOVEMENT_SPEED, 0.01782642796),
+    MOVE_SPEED = new BetterAttribute(MODID + ".moveSpeed", 4.4, 0, new Pair<>(STRENGTH, 0.02), new Pair<>(DEXTERITY, 0.02), new Pair<>(CONSTITUTION, 0.02)).setMCAttribute(SharedMonsterAttributes.MOVEMENT_SPEED, 0.01782642796),
 
     //As a multiplier; 1 for basic start, 4 for maxed
-    ATTACK_SPEED = new AdditiveParentsAttribute(MODID + ".attackSpeed", 100, new Pair<>(STRENGTH, 1d), new Pair<>(DEXTERITY, 2d)),
+    ATTACK_SPEED = new BetterAttribute(MODID + ".attackSpeed", 100, 0, new Pair<>(STRENGTH, 1d), new Pair<>(DEXTERITY, 2d)),
 
     //Interrupt/knockback/trip force should have active modifiers per-attack/skill
-    INTERRUPT_FORCE = new BonusPercentParentsAttribute(MODID + ".interruptForce", 0, STRENGTH),
-            KNOCKBACK_FORCE = new BonusPercentParentsAttribute(MODID + ".knockbackForce", 0, STRENGTH),
-            TRIP_FORCE = new BonusPercentParentsAttribute(MODID + ".tripForce", 0, STRENGTH),
-            PHYSICAL_DAMAGE = new BonusPercentParentsAttribute(MODID + ".physicalDamage", 0, STRENGTH),
+    INTERRUPT_FORCE = new BetterAttribute(MODID + ".interruptForce", 0, 1, STRENGTH),
+            KNOCKBACK_FORCE = new BetterAttribute(MODID + ".knockbackForce", 0, 1, STRENGTH),
+            TRIP_FORCE = new BetterAttribute(MODID + ".tripForce", 0, 1, STRENGTH),
+            PHYSICAL_DAMAGE = new BetterAttribute(MODID + ".physicalDamage", 0, 1, STRENGTH),
             SLASH_DAMAGE = new BetterAttribute(MODID + ".slashDamage", 0, PHYSICAL_DAMAGE),
             PIERCE_DAMAGE = new BetterAttribute(MODID + ".pierceDamage", 0, PHYSICAL_DAMAGE),
             BLUNT_DAMAGE = new BetterAttribute(MODID + ".bluntDamage", 0, PHYSICAL_DAMAGE),
 
     //Many of these will be altered further (often reduced) by modifiers on specific weapons and attacks
-    PROJECTILE_ACCURACY = new AdditiveParentsAttribute(MODID + ".projectileAccuracy", 50, new Pair<>(DEXTERITY, 0.5)),
-            BLOCK_CHANCE = new AdditiveParentsAttribute(MODID + ".block", 50, new Pair<>(DEXTERITY, 0.4)),
-            PARRY_CHANCE = new AdditiveParentsAttribute(MODID + ".parry", 0, new Pair<>(DEXTERITY, 0.1)),
-            DODGE_CHANCE = new AdditiveParentsAttribute(MODID + ".dodge", 50, new Pair<>(DEXTERITY, 0.4)),
-            FINESSE = new AdditiveParentsAttribute(MODID + ".finesse", 0, new Pair<>(DEXTERITY, 0.2)),
-            ARMOR_BYPASS_CHANCE = new BonusPercentParentsAttribute(MODID + ".armorBypass", 0, DEXTERITY),
-            VITAL_STRIKE_CHANCE = new AdditiveParentsAttribute(MODID + ".vitalStrike", 5, new Pair<>(DEXTERITY, 0.5)),
+    PROJECTILE_ACCURACY = new BetterAttribute(MODID + ".projectileAccuracy", 50, 0, new Pair<>(DEXTERITY, 0.5)),
+            BLOCK_CHANCE = new BetterAttribute(MODID + ".block", 50, 0, new Pair<>(DEXTERITY, 0.4)),
+            PARRY_CHANCE = new BetterAttribute(MODID + ".parry", 0, 0, new Pair<>(DEXTERITY, 0.1)),
+            DODGE_CHANCE = new BetterAttribute(MODID + ".dodge", 50, 0, new Pair<>(DEXTERITY, 0.4)),
+            FINESSE = new BetterAttribute(MODID + ".finesse", 0, 0, new Pair<>(DEXTERITY, 0.2)),
+            ARMOR_BYPASS_CHANCE = new BetterAttribute(MODID + ".armorBypass", 0, 1, DEXTERITY),
+            VITAL_STRIKE_CHANCE = new BetterAttribute(MODID + ".vitalStrike", 5, 0, new Pair<>(DEXTERITY, 0.5)),
 
-    HEALTH = new AdditiveParentsAttribute(MODID + ".health", 300, new Pair<>(CONSTITUTION, 2d)).setMCAttribute(SharedMonsterAttributes.MAX_HEALTH, 1),
-            HEALTH_REGEN = new AdditiveParentsAttribute(MODID + ".healthRegen", 0, new Pair<>(CONSTITUTION, 0.1)),
+    HEALTH = new BetterAttribute(MODID + ".health", 300, 0, new Pair<>(CONSTITUTION, 2d)).setMCAttribute(SharedMonsterAttributes.MAX_HEALTH, 1),
+            HEALTH_REGEN = new BetterAttribute(MODID + ".healthRegen", 0, 0, new Pair<>(CONSTITUTION, 0.1)),
             STAMINA = new BetterAttribute(MODID + ".stamina", 100, CONSTITUTION),
-            STAMINA_REGEN = new AdditiveParentsAttribute(MODID + ".staminaRegen", 5, new Pair<>(CONSTITUTION, 0.1)),
+            STAMINA_REGEN = new BetterAttribute(MODID + ".staminaRegen", 5, 0, new Pair<>(CONSTITUTION, 0.1)),
             STABILITY = new BetterAttribute(MODID + ".stability", 100, CONSTITUTION),
             INTERRUPT_STABILITY = new BetterAttribute(MODID + ".interruptStability", 0, STABILITY),
             KNOCKBACK_STABILITY = new BetterAttribute(MODID + ".knockbackStability", 0, STABILITY),
             TRIP_STABILITY = new BetterAttribute(MODID + ".tripStability", 0, STABILITY),
 
-    CHEMICAL_DAMAGE = new BonusPercentParentsAttribute(MODID + ".chemicalDamage", 0, MAGICAL_FORCE),
+    CHEMICAL_DAMAGE = new BetterAttribute(MODID + ".chemicalDamage", 0, 1, MAGICAL_FORCE),
             ACID_DAMAGE = new BetterAttribute(MODID + ".acidDamage", 0, CHEMICAL_DAMAGE),
             BIOLOGICAL_DAMAGE = new BetterAttribute(MODID + ".biologicalDamage", 0, CHEMICAL_DAMAGE),
             HEALING_DAMAGE = new BetterAttribute(MODID + ".healingDamage", 0, BIOLOGICAL_DAMAGE),
             POISON_DAMAGE = new BetterAttribute(MODID + ".poisonDamage", 0, BIOLOGICAL_DAMAGE),
-            ENERGY_DAMAGE = new BonusPercentParentsAttribute(MODID + ".energyDamage", 0, MAGICAL_FORCE),
+            ENERGY_DAMAGE = new BetterAttribute(MODID + ".energyDamage", 0, 1, MAGICAL_FORCE),
             ELECTRIC_DAMAGE = new BetterAttribute(MODID + ".electricDamage", 0, ENERGY_DAMAGE),
             THERMAL_DAMAGE = new BetterAttribute(MODID + ".thermalDamage", 0, ENERGY_DAMAGE),
             HEAT_DAMAGE = new BetterAttribute(MODID + ".heatDamage", 0, THERMAL_DAMAGE),
             COLD_DAMAGE = new BetterAttribute(MODID + ".coldDamage", 0, THERMAL_DAMAGE),
 
     CAST_SUCCESS_CHANCE = new BetterAttribute(MODID + ".castSuccess", 50, MAGICAL_SKILL),
-            DISPEL_CHANCE = new AdditiveParentsAttribute(MODID + ".dispel", 0, new Pair<>(MAGICAL_SKILL, 0.5)),
+            DISPEL_CHANCE = new BetterAttribute(MODID + ".dispel", 0, 0, new Pair<>(MAGICAL_SKILL, 0.5)),
 
     MANA = new BetterAttribute(MODID + ".mana", 100, MAGICAL_CONSTITUTION),
-            MANA_REGEN = new AdditiveParentsAttribute(MODID + ".manaRegen", 5, new Pair<>(MAGICAL_CONSTITUTION, 0.1)),
+            MANA_REGEN = new BetterAttribute(MODID + ".manaRegen", 5, 0, new Pair<>(MAGICAL_CONSTITUTION, 0.1)),
 
-    DEFENSE = new BetterAttribute(MODID + ".defense", 0),
+    DEFENSE = new BetterAttribute(MODID + ".defense"),
             SLASH_DEFENSE = new BetterAttribute(MODID + ".slashDefense", 0, DEFENSE),
             PIERCE_DEFENSE = new BetterAttribute(MODID + ".pierceDefense", 0, DEFENSE),
             FALL_DEFENSE = new BetterAttribute(MODID + ".fallDefense", 0, DEFENSE),
             BLUNT_DEFENSE = new BetterAttribute(MODID + ".bluntDefense", 0, FALL_DEFENSE),
 
-    ELEMENTAL_RESISTANCE = new BetterAttribute(MODID + ".elementalResist", 0),
+    ELEMENTAL_RESISTANCE = new BetterAttribute(MODID + ".elementalResist"),
             CHEMICAL_RESISTANCE = new BetterAttribute(MODID + ".chemicalResist", 0, ELEMENTAL_RESISTANCE),
             ACID_RESISTANCE = new BetterAttribute(MODID + ".acidResist", 0, CHEMICAL_RESISTANCE),
             BIOLOGICAL_RESISTANCE = new BetterAttribute(MODID + ".biologicalResist", 0, CHEMICAL_RESISTANCE),
@@ -106,7 +104,7 @@ public class Attributes
             COLD_RESISTANCE = new BetterAttribute(MODID + ".coldResist", 0, THERMAL_RESISTANCE),
 
     //All of these are determined by the attack/skill being used, and may or may not use other attributes depending
-    RANGE = new BetterAttribute(MODID + ".range", 0), //Determined by the attack/skill being used
+    RANGE = new BetterAttribute(MODID + ".range"), //Determined by the attack/skill being used
             MELEE_RANGE = new BetterAttribute(MODID + ".meleeRange", 0, RANGE),
             MIN_MELEE_RANGE = new BetterAttribute(MODID + ".minMeleeRange", 0, MELEE_RANGE),
             MAX_MELEE_RANGE = new BetterAttribute(MODID + ".maxMeleeRange", 0, MELEE_RANGE),
@@ -115,22 +113,22 @@ public class Attributes
             MAX_PROJECTILE_RANGE = new BetterAttribute(MODID + ".maxProjectileRange", 0, PROJECTILE_RANGE),
 
     //All of these are determined by the attack/skill being used, and may or may not use other attributes depending
-    MAX_TARGETS = new BetterAttribute(MODID + ".maxTargets", 0),
+    MAX_TARGETS = new BetterAttribute(MODID + ".maxTargets"),
             MAX_MELEE_TARGETS = new BetterAttribute(MODID + ".maxMeleeTargets", 0, MAX_TARGETS), //Determined by the attack/skill being used, and possibly strength/dexterity/magical force/magical skill
             MAX_PROJECTILE_TARGETS = new BetterAttribute(MODID + ".maxProjectileTargets", 0, MAX_TARGETS), //Determined by the attack/skill being used
 
     //All of these are determined by the weapons/attack/skill being used, and may or may not use other attributes depending
-    MAX_MELEE_ANGLE = new BetterAttribute(MODID + ".maxAngle", 0),
+    MAX_MELEE_ANGLE = new BetterAttribute(MODID + ".maxAngle"),
             COMBO = new BetterAttribute(MODID + ".combo", 100),
-            PROJECTILE_COUNT = new BetterAttribute(MODID + ".projectileCount", 0),
-            PROJECTILE_SPEED = new BetterAttribute(MODID + ".projectileSpeed", 0),
-            PROJECTILE_DURATION = new BetterAttribute(MODID + ".projectileDuration", 0),
-            AOE_DURATION = new BetterAttribute(MODID + ".aoeDuration", 0),
+            PROJECTILE_COUNT = new BetterAttribute(MODID + ".projectileCount"),
+            PROJECTILE_SPEED = new BetterAttribute(MODID + ".projectileSpeed"),
+            PROJECTILE_DURATION = new BetterAttribute(MODID + ".projectileDuration"),
+            AOE_DURATION = new BetterAttribute(MODID + ".aoeDuration"),
 
     //Intelligence and Wisdom are terrible gameplay concepts *for players*...they work fine for NPCs, ONLY FOR DECIDING WHETHER AI MAKES GOOD DECISIONS, and you really only need one for that
     //On the other hand, memory is an often-overlooked concept for AI
-    AI_INTELLIGENCE = new BetterAttribute(MODID + ".aiIntelligence", 0),
-            AI_MEMORY = new BetterAttribute(MODID + ".aiMemory", 0);
+    AI_INTELLIGENCE = new BetterAttribute(MODID + ".aiIntelligence"),
+            AI_MEMORY = new BetterAttribute(MODID + ".aiMemory");
 
     //Charisma is a terrible gameplay concept no matter what you apply it to, and I'm not using it period.
 
